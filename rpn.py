ed8d56fd704b46ffe9c73c8a6c461dffb7467118
nnathann <75263433+nnathann@users.noreply.github.com>
2023-05-15 11:45:09 +0100

    Update rpn.py
    added comments to code

--- rpn.py
+++ rpn.py
@@ -1,5 +1,6 @@
 import math
 
+# Define the supported operators and their corresponding functions
 operators = {
     '+': lambda x, y: x + y,
     '-': lambda x, y: x - y,
@@ -12,26 +13,38 @@ operators = {
     'mod': lambda x, y: x % y
 }
 
+# Open the input file
 with open('input.txt') as f:
+    # Process each line in the file
     for line in f:
-        stack = []
+        stack = []  # Create an empty stack to store values
+        # Split the line into individual tokens and process each token
         for token in line.strip().split():
-            if token in operators:
+            if token in operators:  # If the token is an operator
                 try:
+                    # Apply the operator to the last two values in the stack
+                    # and append the result to the stack
                     stack.append(operators[token](*stack[-2:]))
+                    # Remove the last two values from the stack
                     stack = stack[:-2]
                 except:
+                    # Handle errors when applying the operator
                     print(f"Error: Not enough values for '{token}'.")
                     break
-            else:
+            else:  # If the token is not an operator (assume it's a number)
                 try:
+                    # Convert the token to a float and append it to the stack
                     stack.append(float(token))
                 except:
+                    # Handle errors when converting the token to a float
                     print(f"Error: Invalid token '{token}'.")
                     break
         if len(stack) == 1:
+            # If there is exactly one value in the stack, print it as the result
             print(f"{line.strip()} = {stack[0]}")
         elif len(stack) > 1:
+            # If there are more than one value in the stack, it's an error
             print("Error: Too many values in the stack.")
         else:
+            # If there are no values in the stack, it's an error
             print("Error: No values in the stack.")
